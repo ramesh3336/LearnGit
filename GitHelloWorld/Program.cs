e4daa7187903c5cd998b3245dfc4e582636e18e3
Ramesh Bhati <rameshbhati3336@outlook.com>
2019-11-02 02:13:29 +0530

    Added Project

---
+++ GitHelloWorld/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GitHelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
