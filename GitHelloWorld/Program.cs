36d2501ce3613a2864989769d17183385a764cd0
Ramesh Bhati <rameshbhati3336@outlook.com>
2019-11-02 15:42:57 +0530

    commit changes in mybrnach

--- GitHelloWorld/Program.cs
+++ GitHelloWorld/Program.cs
@@ -6,7 +6,7 @@ namespace GitHelloWorld
     {
         static void Main(string[] args)
         {
-            //New branch MyFirstBranch is created
+            //New branch MyFirstBranch is created in my branch
             Console.WriteLine("Hello Git World !!!!!!! ");
             Console.WriteLine("this is Developer 2  dsd");
         }
